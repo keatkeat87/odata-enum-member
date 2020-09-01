1e284fab4a94450d3b2499c9890028bebc60774a
Derrick Yam <keatkeat87@live.com>
2020-09-01 18:34:12 +0800

    ok

---
+++ odata-enum-member-issue/WeatherForecast.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Runtime.Serialization;
+
+namespace odata_route_constraint_issue
+{
+    public enum Status { 
+        [EnumMember(Value = "Sold out")]
+        SoldOut
+    }
+    public class WeatherForecast
+    {
+        public Guid Id { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public Status status { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
